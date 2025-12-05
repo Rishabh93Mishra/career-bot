d949e089bf35c8ee99afd83bc2fb3bf0602a1a25
Rishbah93Mishra <24150cn104@glbitm.ac.in>
2025-12-05 09:05:25 +0530

    Fix config.py with get_db

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-DB_CONFIG = {
-    'host': "bd27lkuu8i4kogikfal-mysql.services.clever-cloud.com",
-    'user': "uwubzwymacmo0ji",
-    'password': "Rishabh*1",
-    'database': "bd27lkuu8i4kogikfal",
-    'port': 3306
-}
+import mysql.connector
 
+def get_db():
+    return mysql.connector.connect(
+        host="bd27lkuu8i4kogikfal-mysql.services.clever-cloud.com",
+        user="uvwbazynwmacmo0ji",
+        password="Rishabh*1",
+        database="bd27lkuu8i4kogikfal",
+        port=3306
+    )
