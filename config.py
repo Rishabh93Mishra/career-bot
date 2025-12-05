41623bd5f48db332d8103f7e0c318f569e38d76f
Rishbah93Mishra <24150cn104@glbitm.ac.in>
2025-12-05 09:12:26 +0530

    Added correct password

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_db():
     return mysql.connector.connect(
         host="bd27lkuu8i4kogikfal-mysql.services.clever-cloud.com",
         user="uvwbazynwmacmo0ji",
-        password="Rishabh*1",
+        password="Ee8SNwtphk1RZGlxVySW",
         database="bd27lkuu8i4kogikfal",
         port=3306
     )
