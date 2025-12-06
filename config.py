423266a29fa721a73cbc9fad121afbc0e34fd641
Rishbah93Mishra <24150cn104@glbitm.ac.in>
2025-12-06 20:00:18 +0530

    Fix correct Clever Cloud host

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="bd27lkuu8i4kogikfal-mysql.services.clever-cloud.com",
-        user="uvwbazynwmacmo0ji",
+        host="bdz27lkwu8ik0qoikfal-mysql.services.clever-cloud.com",
+        user="uvubwzynwacmo0ji",
         password="Ee8SNwtphk1RZGlxVySW",
-        database="bd27lkuu8i4kogikfal",
+        database="bdz27lkwu8ik0qoikfal",
         port=3306
     )
